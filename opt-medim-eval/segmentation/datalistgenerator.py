4321cbef6d7ad4cfb9c23359e2525ac06156384f
Joseph Johnson <sc21jj@leeds.ac.uk>
2025-03-19 12:30:04 +0000

    [0] Initial push to Git: Created working datalistgenerator.py file, tested on 90-10 and 90-5 GAN-MRI mixed directories to produce corresponding datalist .json files and wrote task .yaml files for each datalist

---
+++ opt-medim-eval/segmentation/datalistgenerator.py
@@ -0,0 +1,61 @@
+import os
+import json
+import random
+import shutil
+import tempfile
+import argparse
+
+def generate_datalist(dscategory, realpct, dataroot):
+
+    test_dir = os.path.join(dataroot, "Test/")
+    train_dir = os.path.join(dataroot, "Training/")
+    label_dir = os.path.join(dataroot, "Labels/")
+
+    datalist_json = {"testing": [], "training": []}
+
+    # Populate JSON with testing images
+    datalist_json["testing"] = [
+        {"image": "./Test/" + file} for file in os.listdir(test_dir) if (".nii" in file) and ("._" not in file)
+    ]
+
+    # Populate JSON with training images and labels
+    datalist_json["training"] = [
+        {"image": "./Training/" + file, "label": "./Labels/" + file, "fold": 0}
+        for file in os.listdir(train_dir)
+        if (".nii" in file) and ("._" not in file)
+    ]  # Initialize as single fold
+
+    datalist_json["training"][:10]
+
+    # Randomise training data
+    random.seed(42)
+    random.shuffle(datalist_json["training"])
+
+    # Split training data into N random folds
+    num_folds = 5
+    fold_size = len(datalist_json["training"]) // num_folds
+    for i in range(num_folds):
+        for j in range(fold_size):
+            datalist_json["training"][i * fold_size + j]["fold"] = i
+
+    # Format datalist file name
+    synthpct = 100 - realpct
+    datalist_file = f"{dscategory}{realpct}-{synthpct}.json"
+
+    # Obtain and construct filepath for datalist
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    datalist_filepath = f"{root_dir}/datalists/{datalist_file}"
+
+    # Save JSON to file
+    with open(datalist_filepath, "w", encoding="utf-8") as f:
+        json.dump(datalist_json, f, ensure_ascii=False, indent=4)
+    print(f"Datalist is saved to {datalist_filepath}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate formatted datalist for Auto3DSeg AutoRunner")
+    parser.add_argument("--ds_category", type=str, required=True, help="Dataset category being sampled from [ganmri/ganct/dmmri/dmct]")
+    parser.add_argument("--percent_real", type=int, required=True, help="Percentage of real imaging data in mixed dataset")
+    parser.add_argument("--root_dir", type=str, required=True, help="Directory containing the test and training subdirectories")
+    args = parser.parse_args()
+    
+    generate_datalist(args.ds_category, args.percent_real, args.root_dir)
